384d21566496a47f0df500eb30b91f6aa1e34e81
Ilia Kopylov <ilia.kopylov@jetbrains.com>
2025-06-26 18:09:08 +0200

    [NP-1873-15] Add ability to work with different RwModes for ZipArchiveStorage

---
+++ Core/src/Impl/Storages/ZipHelpers/ZipArchiveContainer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace JetBrains.SymbolStorage.Impl.Storages.ZipHelpers
+{
+  internal sealed class ZipArchiveContainer: IDisposable
+  {
+    public static ZipArchiveContainer Open(string archivePath, ZipArchiveMode mode)
+    {
+      DateTimeOffset openedAt = DateTimeOffset.Now; // Get time before opening the archive
+      return new ZipArchiveContainer(archivePath, ZipFile.Open(archivePath, mode), openedAt);
+    }
+    
+    private readonly string myArchivePath;
+    private readonly ZipArchiveMode myArchiveMode;
+    
+    private ZipArchiveContainer(string archivePath, ZipArchive archive, DateTimeOffset openedAt)
+    {
+      myArchivePath = archivePath;
+      myArchiveMode = archive.Mode;
+      Archive = archive;
+      OpenedAt = openedAt;
+      DirtyBytes = 0;
+    }
+    
+    public ZipArchive Archive { get; private set; }
+    public DateTimeOffset OpenedAt { get; private set; }
+    public long DirtyBytes { get; private set; }
+    
+
+    public void AddDirtyBytes(long bytes)
+    {
+      Debug.Assert(bytes >= 0);
+      DirtyBytes += bytes;
+    }
+
+    public async Task WriteToArchiveAsync(ZipArchiveEntry entry, Stream data)
+    {
+      long initialPosition = data.Position;
+      await using (var targetStream = entry.Open())
+      {
+        await data.CopyToAsync(targetStream);
+      }
+      
+      AddDirtyBytes(data.Position - initialPosition);
+    }
+
+    /// <summary>
+    /// Reopens archive file
+    /// </summary>
+    /// <remarks>
+    /// In <c>ZipArchiveMode.Update</c> the archive stores all data in memory,
+    /// thus at some point it is better to sync its content to the disk.
+    /// This can be done by reopening the archive file
+    /// </remarks>
+    public void Reopen()
+    {
+      Archive.Dispose();
+      OpenedAt = DateTimeOffset.Now; // Get time before opening the archive
+      Archive = ZipFile.Open(myArchivePath, myArchiveMode);
+      DirtyBytes = 0;
+    }
+
+    public void Dispose()
+    {
+      Archive.Dispose();
+    }
+  }
+}
--- Core/src/Impl/Storages/ZipHelpers/ZipArchiveGuard.cs
+++ Core/src/Impl/Storages/ZipHelpers/ZipArchiveGuard.cs
@@ -1,17 +1,57 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.IO.Compression;
+using System.Threading.Tasks;
 
 namespace JetBrains.SymbolStorage.Impl.Storages.ZipHelpers
 {
-  internal struct ZipArchiveGuard(ZipArchive archive, ZipArchiveProvider provider) : IDisposable
+  internal struct ZipArchiveGuard(ZipArchiveContainer archiveContainer, ZipArchiveProvider provider) : IDisposable
   {
     private ZipArchiveProvider? myProvider = provider;
 
-    public ZipArchive Archive { get; } = archive;
+    public ZipArchive Archive => archiveContainer.Archive;
 
+    public bool IsNewlyCreatedEntry(ZipArchiveEntry entry)
+    {
+      return entry.LastWriteTime >= archiveContainer.OpenedAt;
+    }
+
+    /// <summary>
+    /// Returns the length of the entry
+    /// </summary>
+    /// <remarks>
+    /// If archive in <c>ZipArchiveMode.Update</c> mode and entry is newly created then <c>entry.Length</c> is not available.
+    /// In this mode the archive is in memory, so it is safe to open the stream and get its length
+    /// </remarks>
+    public long GetEntryLength(ZipArchiveEntry entry)
+    {
+      Debug.Assert(myProvider != null);
+      if (myProvider.Mode == ZipArchiveMode.Update && IsNewlyCreatedEntry(entry))
+      {
+        using (var stream = entry.Open())
+        {
+          return stream.Length;
+        }
+      }
+      
+      return entry.Length;
+    }
+
+    public void AddDirtyBytes(long bytes)
+    {
+      Debug.Assert(myProvider != null);
+      archiveContainer.AddDirtyBytes(bytes);
+    }
+    public Task WriteToArchiveAsync(ZipArchiveEntry entry, Stream data)
+    {
+      Debug.Assert(myProvider != null);
+      return archiveContainer.WriteToArchiveAsync(entry, data);
+    }
+    
     public void Dispose()
     {
-      myProvider?.Release(Archive);
+      myProvider?.Release(archiveContainer);
       myProvider = null;
     }
   }
