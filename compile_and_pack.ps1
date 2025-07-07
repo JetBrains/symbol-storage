0ac0b6dac7ec12758ef3197a4eceb08fd8dd9f2d
Ilia Kopylov <ilia.kopylov@jetbrains.com>
2025-07-07 13:57:41 +0200

    [NP-1873-18] Automatic installation of dotnet from build script

--- compile_and_pack.ps1
+++ compile_and_pack.ps1
@@ -4,53 +4,83 @@
 
 Set-StrictMode -Version Latest
 $ErrorActionPreference=[System.Management.Automation.ActionPreference]::Stop
+$ProgressPreference="SilentlyContinue"
 
 
-[xml]$_Project=Get-Content Common.targets
-$_Framework=$_Project.Project.PropertyGroup.TargetFramework
-$_PackageVersion=$_Project.Project.PropertyGroup.Version
-$_PublishDir="publish"
+[xml]$Project=Get-Content Common.targets
+$Framework=$Project.Project.PropertyGroup.TargetFramework
+$PackageVersion=$Project.Project.PropertyGroup.Version
+$PublishDir="$PSScriptRoot\publish"
 
-Write-Host "Framework:" $_Framework
-Write-Host "PackageVersion:" $_PackageVersion
-Write-Host "Publish directory:" $_PublishDir
+$DotNetVersion="9.0"
+$DotNetCustomInstallationDir="$env:LOCALAPPDATA\JetBrains\dotnet-sdk-temp\1e63e382e732473eab7845c59486bf30"
+$DotNet="$DotNetCustomInstallationDir\dotnet.exe"
+
+Write-Host "Framework:" $Framework
+Write-Host "PackageVersion:" $PackageVersion
+Write-Host "Publish directory:" $PublishDir
 
 
-function packNuget($_Name, $_Runtime) {
-  $_Template= Get-Content -Encoding UTF8 "NugetPackProjectTemplate.csproj.template"
-  $_Template = $_Template -replace "{{ROOT_PATH}}", ".."
-  $_Template = $_Template -replace "{{NAME}}", $_Name
-  $_Template = $_Template -replace "{{RUNTIME}}", $_Runtime
-  $_Template = $_Template -replace "{{CURRENT_YEAR}}", $(get-date -Format yyyy)
+function installDotNet() {
+  try {
+    $DotNetInstalled=(Get-Command dotnet).Path
+    if ((. $DotNetInstalled --list-sdks) -match "^$([Regex]::Escape($DotNetVersion))") {
+      Write-Host "System .NET $DotNetVersion will be used (location: $DotNetInstalled)"
+      return $DotNetInstalled
+    }
+  } catch {
+  }
+  
+  Write-Host ".NET $DotNetVersion will be installed (location: $DotNetCustomInstallationDir)"
+  if(!(Test-Path -PathType Container "$PublishDir"))
+  {
+    New-Item -ItemType Directory -Path "$PublishDir"
+  }
+  Invoke-WebRequest 'https://dot.net/v1/dotnet-install.ps1' -OutFile "$PublishDir\dotnet-install.ps1"
+  if (([regex]::Matches($DotNetVersion, "\." )).count -le 1) {
+    . "$PublishDir\dotnet-install.ps1" -InstallDir $DotNetCustomInstallationDir -Channel $DotNetVersion -NoPath
+  } else {
+    . "$PublishDir\dotnet-install.ps1" -InstallDir $DotNetCustomInstallationDir -Version $DotNetVersion -NoPath
+  }
+  return $DotNet
+}
 
-  $_CsprojSpec="$_PublishDir\Package.$_Name.$_Runtime.csproj"
-  Out-File -InputObject $_Template -Encoding utf8 $_CsprojSpec
-  dotnet pack $_CsprojSpec --output "$_PublishDir\nuget\" --artifacts-path "$_PublishDir\NugetBuild\$_Name\$_Runtime\" 
+
+function packNuget($Name, $Runtime) {
+  $Template= Get-Content -Encoding UTF8 "NugetPackProjectTemplate.csproj.template"
+  $Template = $Template -replace "{{ROOT_PATH}}", ".."
+  $Template = $Template -replace "{{NAME}}", $Name
+  $Template = $Template -replace "{{RUNTIME}}", $Runtime
+  $Template = $Template -replace "{{CURRENT_YEAR}}", $(get-date -Format yyyy)
+
+  $CsprojSpec="$PublishDir\Package.$Name.$Runtime.csproj"
+  Out-File -InputObject $Template -Encoding utf8 $CsprojSpec
+  . $DotNet pack $CsprojSpec --output "$PublishDir\nuget\" --artifacts-path "$PublishDir\NugetBuild\$Name\$Runtime\" 
   
   if (0 -ne $LastExitCode) {
     throw "dotnet pack exited with error"
   }
 }
 
-function packZipArchive($_Name, $_Runtime) {
-  if(!(Test-Path -PathType Container "$_PublishDir\archive\"))
+function packZipArchive($Name, $Runtime) {
+  if(!(Test-Path -PathType Container "$PublishDir\archive\"))
   {
-    New-Item -ItemType Directory -Path "$_PublishDir\archive\"
+    New-Item -ItemType Directory -Path "$PublishDir\archive\"
   }
-  Compress-Archive -Path "$_PublishDir\$_Name\$_Runtime\*" -DestinationPath "$_PublishDir\archive\JetBrains.SymbolStorage.$_Name.$_Runtime.zip"
+  Compress-Archive -Path "$PublishDir\$Name\$Runtime\*" -DestinationPath "$PublishDir\archive\JetBrains.SymbolStorage.$Name.$Runtime.zip"
 }
 
-function packTarArchive($_Name, $_Runtime) {
-  If(!(Test-Path -PathType Container "$_PublishDir\archive\"))
+function packTarArchive($Name, $Runtime) {
+  If(!(Test-Path -PathType Container "$PublishDir\archive\"))
   {
-    New-Item -ItemType Directory -Path "$_PublishDir\archive\"
+    New-Item -ItemType Directory -Path "$PublishDir\archive\"
   }
-  Write-Host "$_PublishDir\$_Name\$_Runtime\"
+  Write-Host "$PublishDir\$Name\$Runtime\"
   
-  $_Location= Get-Location
+  $Location= Get-Location
   Push-Location
-  cd "$_PublishDir\$_Name\$_Runtime"
-  tar -czvf "$_Location\$_PublishDir\archive\JetBrains.SymbolStorage.$_Name.$_Runtime.tar.gz" "."
+  cd "$PublishDir\$Name\$Runtime"
+  tar -czvf "$PublishDir\archive\JetBrains.SymbolStorage.$Name.$Runtime.tar.gz" "."
   Pop-Location
   
   if (0 -ne $LastExitCode) {
@@ -58,24 +88,27 @@ function packTarArchive($_Name, $_Runtime) {
   }
 }
 
-function packArchive($_ArchType, $_Name, $_Runtime) {
-  switch ($_ArchType) {
-    "tar" { packTarArchive $_Name $_Runtime }
-    "zip" { packZipArchive $_Name $_Runtime }
+function packArchive($ArchType, $Name, $Runtime) {
+  switch ($ArchType) {
+    "tar" { packTarArchive $Name $Runtime }
+    "zip" { packZipArchive $Name $Runtime }
     default { throw "Unknown archive type" }
   }
 }
 
-function compileAndPack($_Runtime, $_ArchType) {
-  Write-Host "Compile and pack for $_Runtime"
+function compileAndPack($Runtime, $ArchType) {
+  Write-Host "Compile and pack for $Runtime"
 
-  dotnet publish -f $_Framework -r $_Runtime -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -warnAsMessage:IL2104 -o "$_PublishDir\Manager\$_Runtime" Manager
-  dotnet publish -f $_Framework -r $_Runtime -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -warnAsMessage:IL2104 -o "$_PublishDir\Uploader\$_Runtime" Uploader
-  packNuget Manager $_Runtime
-  packNuget Uploader $_Runtime
-  packArchive $_ArchType Manager $_Runtime
-  packArchive $_ArchType Uploader $_Runtime
+  . $DotNet publish -f $Framework -r $Runtime -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -warnAsMessage:IL2104 -o "$PublishDir\Manager\$Runtime" Manager
+  . $DotNet publish -f $Framework -r $Runtime -c Release --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=true -warnAsMessage:IL2104 -o "$PublishDir\Uploader\$Runtime" Uploader
+  packNuget Manager $Runtime
+  packNuget Uploader $Runtime
+  packArchive $ArchType Manager $Runtime
+  packArchive $ArchType Uploader $Runtime
 }
+
+
+$DotNet = installDotNet
 
 compileAndPack "linux-arm" "tar"
 compileAndPack "linux-arm64" "tar"
